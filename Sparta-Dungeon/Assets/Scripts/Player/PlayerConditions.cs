2428f0cdc4b6488b7fdafb55f064fbf76ea38714
PostMail-hub <gmlals2949@naver.com>
2025-05-22 15:19:29 +0900

    feat: 컨디션 관리 기능 추가
    - 체력 값을 관리해주는 함수 추가
    - 사망 시 게임 시간을 0으로 멈추는 함수 추가
    - 체력바가 실시간으로 체력 값에 따라 변화하는 함수 추가

---
+++ Sparta-Dungeon/Assets/Scripts/Player/PlayerConditions.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        if (health.curValue <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Die()
+    {
+        Time.timeScale = 0;
+    }
+}
