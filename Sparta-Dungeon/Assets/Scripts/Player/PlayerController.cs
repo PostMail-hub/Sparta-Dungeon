28dedf242b938f7abbb7c0400ed267837b2e6ddd
PostMail-hub <gmlals2949@naver.com>
2025-05-22 11:45:56 +0900

    feat: 플레이어 움직임 구현
    - WASD 에 맞춰서 이동하는 기능 추가
    - 스페이스바 클릭 시 점프 기능 추가
    - 마우스를 움직이면 화면도 동일하게 움직이는 기능 추가

--- Sparta-Dungeon/Assets/Scripts/Player/PlayerController.cs
+++ Sparta-Dungeon/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,121 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("플레이어 움직임")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumpPower;
+    public LayerMask groundLayerMask;
+
+    [Header("플레이어 카메라")]
+    public Transform cameraContainer;
+    public float minXLook; 
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+
+    private Vector2 mouseDelta;
+
+    [HideInInspector]
+    public bool canLook = true;
+
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        if (canLook)
+        {
+            CameraLook();
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context) // 마우스를 움직일 때 화면도 같이 움직임
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context) // 키 입력 시 알맞게 움직이기
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context) // 키 입력 시 조건에 맞다면 점프
+    {
+        if (context.phase == InputActionPhase.Started && IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    private void Move() // 캐릭터를 움직이는 함수
+    {
+
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed; 
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir;
+    }
+
+    void CameraLook() // 카메라를 움직이는 함수
+    {
+
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    bool IsGrounded() // 아래에 짧게 레이를 쏴서 바닥인지 아닌지 체크하기 
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.1f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.1f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.1f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) +(transform.up * 0.1F), Vector3.down)
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.5f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void ToggleCursor(bool toggle)
+    {
+        Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
+        canLook = !toggle;
     }
 }
